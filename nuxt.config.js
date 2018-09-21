d7094e0d811d1e249a3e5525bedeaecb9976fa26
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2018-09-21 10:05:25 +0100

    Updated static site config

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,13 +6,11 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Depictions of Addiction explores how photographers represent addiction through visual work.' }
     ],
-    metaInfo: {
-      link: [
-        { rel: 'stylesheet', href: 'https://api.tiles.mapbox.com/mapbox-gl-js/v0.49.0/mapbox-gl.css' },
-        { rel: 'script', src: 'https://api.tiles.mapbox.com/mapbox-gl-js/v0.49.0/mapbox-gl.js' },
-        { rel: 'icon', type: 'image/x-icon', href: 'favicon.png' }
-      ]
-    }
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: 'favicon.png' },
+      { rel: 'stylesheet', href: 'https://api.tiles.mapbox.com/mapbox-gl-js/v0.49.0/mapbox-gl.css' },
+      { rel: 'script', src: 'https://api.tiles.mapbox.com/mapbox-gl-js/v0.49.0/mapbox-gl.js' }
+    ]
   },
   generate: {
     dir: 'docs'
